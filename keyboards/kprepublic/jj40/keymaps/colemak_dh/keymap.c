ad259d9e4e93ad3fcade2f7cd7390fc9c2c23753
Kim Schmider <hello@schmider.kim>
2025-01-20 22:50:27 +0100

    Swap keys

--- keyboards/kprepublic/jj40/keymaps/colemak_dh/keymap.c
+++ keyboards/kprepublic/jj40/keymaps/colemak_dh/keymap.c
@@ -126,48 +126,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  Tab |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  |   ;  |   -  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |  Esc |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  | Enter|
+ * |  Esc |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  |   '  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Bksp |   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |   '  |
+ * | Bksp |   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Super|  Alt |      | Lower| Shift|    Space    | Raise| Intrl|      |  Alt | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
   [DEF] = LAYOUT_planck_mit(KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
-                            KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
-                            KC_BSPC, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+                            KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+                            KC_BSPC, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                             KC_LGUI, KC_LALT, XXXXXXX, MO(LWR), KC_LSFT,     KC_SPC,       MO(RSE), MO(INT), XXXXXXX, KC_RALT, KC_LCTL),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   +  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      | S+F1 | S+F2 | S+F3 | S+F4 | S+F5 | S+F6 | Home | PgDn | PgUp |  End |      |
+ * |      | S+F1 | S+F2 | S+F3 | S+F4 | S+F5 | S+F6 | Home | PgDn | PgUp |  End |   |  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  Del | S+F7 | S+F8 | S+F9 | S+F10| S+F11| S+F12|      |   {  |   }  |   |  |      |
+ * |  Del | S+F7 | S+F8 | S+F9 | S+F10| S+F11| S+F12|      |      |   {  |   }  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-  [LWR] = LAYOUT_planck_mit(KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,   KC_ASTR,   KC_LPRN, KC_RPRN,   KC_PLUS,
-                            _______,  S(KC_F1), S(KC_F2), S(KC_F3), S(KC_F4), S(KC_F5), S(KC_F6), KC_HOME,   KC_PGDN,   KC_PGUP, KC_END,    _______,
-                            KC_DEL,   S(KC_F7), S(KC_F8), S(KC_F9), S(KC_F10),S(KC_F11),S(KC_F12),_______,   KC_LCBR,   KC_RCBR, KC_PIPE,   _______,
-                            _______,  _______,  _______,  _______,  _______,       _______,       _______,   _______,   _______, _______,   _______),
+  [LWR] = LAYOUT_planck_mit(KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,   KC_LPRN, KC_RPRN, KC_PLUS,
+                            _______,  S(KC_F1), S(KC_F2), S(KC_F3), S(KC_F4), S(KC_F5), S(KC_F6), KC_HOME,  KC_PGDN,   KC_PGUP, KC_END,  KC_PIPE,
+                            KC_DEL,   S(KC_F7), S(KC_F8), S(KC_F9), S(KC_F10),S(KC_F11),S(KC_F12),_______,  _______,   KC_LCBR, KC_RCBR, _______,
+                            _______,  _______,  _______,  _______,  _______,       _______,       _______,  _______,   _______, _______, _______),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   =  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | Left | Down |  Up  | Right|      |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | Left | Down |  Up  | Right|   \  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  Del |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |   [  |   ]  |   \  |      |
+ * |  Del |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |   [  |   ]  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-  [RSE] = LAYOUT_planck_mit(KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
-                            _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-                            KC_DEL,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LBRC, KC_RBRC, KC_BSLS, _______,
+  [RSE] = LAYOUT_planck_mit(KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+                            _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSLS,
+                            KC_DEL,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_LBRC, KC_RBRC, _______,
                             _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, _______),
 
 /* Adjust (Lower + Raise)
